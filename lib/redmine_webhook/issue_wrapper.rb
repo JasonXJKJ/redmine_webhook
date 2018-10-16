54daa0030e7889040b6b3845474aac1393c2dffd
JasonXJKJ <jason@startrails.cn>
2018-10-16 11:28:34 +0800

    fix bug

--- lib/redmine_webhook/issue_wrapper.rb
+++ lib/redmine_webhook/issue_wrapper.rb
@@ -7,17 +7,17 @@ module RedmineWebhook
 
     def to_new
       if @issue.assigned_to == nil
-        @issue.author.lastname + @issue.author.firstname + ' 创建 ' + @issue.tracker.name + ' : ' + @issue.subject + '。紧急程度: ' + @issue.priority.name
+        @issue.author.lastname + @issue.author.firstname + ' 创建 ' + @issue.tracker.name + ': ' + @issue.subject + '。紧急程度: ' + @issue.priority.name
       else
-        @issue.author.lastname + @issue.author.firstname + ' 创建 ' + @issue.tracker.name + ' : ' + @issue.subject + '。指派给: ' + @issue.assigned_to.lastname + @issue.assigned_to.firstname + '。紧急程度: ' + @issue.priority.name
+        @issue.author.lastname + @issue.author.firstname + ' 创建 ' + @issue.tracker.name + ': ' + @issue.subject + '。指派给: ' + @issue.assigned_to.lastname + @issue.assigned_to.firstname + '。紧急程度: ' + @issue.priority.name
       end
     end
 
     def to_update
       if @issue.assigned_to == nil
-        @journal.user.lastname + @journal.user.firstname + ' 修改 ' + @issue.tracker.name + ' : ' + @issue.subject + '。紧急程度: ' + @issue.priority.name + '。状态: ' + @issue.status.name
+        @journal.user.lastname + @journal.user.firstname + ' 修改 ' + @issue.tracker.name + ': ' + @issue.subject + '。紧急程度: ' + @issue.priority.name + '。状态: ' + @issue.status.name
       else
-        @journal.user.lastname + @journal.user.firstname + ' 修改 ' + @issue.tracker.name + ' : ' + @issue.subject + '。指派给: ' + @issue.assigned_to.lastname + @issue.assigned_to.firstname + '。紧急程度: ' + @issue.priority.name + '。状态: ' + @issue.status.name
+        @journal.user.lastname + @journal.user.firstname + ' 修改 ' + @issue.tracker.name + ': ' + @issue.subject + '。指派给: ' + @issue.assigned_to.lastname + @issue.assigned_to.firstname + '。紧急程度: ' + @issue.priority.name + '。状态: ' + @issue.status.name
       end
     end
   end
